1fc198d583469c8035b72fcb57b30f905b28428f
Nguyễn Trọng An <nguyentrongan010@gmail.com>
2019-03-25 14:13:43 +0700

    Add config db for deploying

---
+++ src/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    database: {
+        user: 'farwxffx',
+        host: '	isilo.db.elephantsql.com',
+        database: 'farwxffx',
+        password: 'bh1Fy3VOocV0rOPGRtVTe9qh4NOcFubO',
+        port: 5432
+    },
+    jwt_key: 'sdjflosk-sdlkfj!dlkfj./dlkfj~'
+}
